a1a8be77c186eefb5177fb766d42c1bb7656fc7c
0118Add <60157610+0118Add@users.noreply.github.com>
2021-06-16 02:47:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,6 @@ sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 #rm -rf package/lean/luci-app-dockerman
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 #git clone https://github.com/0118Add/sirpdboy-package.git package/sirpdboy-package
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/brook package/brook
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/chinadns-ng
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/trojan-go
