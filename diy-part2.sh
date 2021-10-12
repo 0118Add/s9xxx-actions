1ba35a940c66934a59d6e4dd74c824227136383e
0118Add <60157610+0118Add@users.noreply.github.com>
2021-10-13 00:13:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -65,7 +65,7 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 #git clone https://github.com/8688Add/luci-app-vssr-plus.git package/luci-app-vssr-plus
 #git clone https://github.com/bin20088/luci-app-koolddns.git package/luci-app-koolddns
-#git clone https://github.com/tuanqing/install-program.git package/install-program
+git clone https://github.com/kuoruan/openwrt-upx package/openwrt-upx
 git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 rm -rf package/lean/luci-app-frpc
