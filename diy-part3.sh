c3a04c039fdbe38b8d9e4a706a465f7fcb955b06
0118Add <60157610+0118Add@users.noreply.github.com>
2022-08-21 14:56:49 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -8,10 +8,10 @@
 
 
 # 更换5.4内核
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.19/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/armvirt/Makefile
 
 # 更改主机名
-sed -i "s/hostname='.*'/hostname='X86'/g" package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='N1'/g" package/base-files/files/bin/config_generate
 
 # 内核替换成 kernel 5.4.xxx
 #sed -i 's/LINUX_KERNEL_HASH-5.4.203 = fc933f5b13066cfa54aacb5e86747a167bad1d8d23972e4a03ab5ee36c29798a/LINUX_KERNEL_HASH-5.4.210 = 940396878c2c183531669d87831eda60a86fbf4662904922c49151b50afc888e/g' ./include/kernel-5.4
@@ -21,7 +21,9 @@ sed -i "s/hostname='.*'/hostname='X86'/g" package/base-files/files/bin/config_ge
 # sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
 git clone https://github.com/gd0772/package package/gd772
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argonne package/luci-theme-argonne
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
-wget https://raw.githubusercontent.com/0118Add/patch/main/gd772.sh
-bash gd772.sh
+wget https://raw.githubusercontent.com/0118Add/patch/main/n1.sh
+bash n1.sh
