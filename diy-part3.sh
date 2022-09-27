2854efebd943fb8614f2e1024bfa7b7bcfa43a31
0118Add <60157610+0118Add@users.noreply.github.com>
2022-09-27 09:21:15 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -41,6 +41,9 @@ sed -i "s|https.*/s9xxx-openwrt|https://github.com/0118Add/N1dabao|g" package/am
 sed -i "s|opt/kernel|https://github.com/breakings/OpenWrt/opt/kernel|g" package/amlogic/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|s9xxx_lede|g" package/amlogic/luci-app-amlogic/root/etc/config/amlogic
 
+# 修改 bypass 依赖
+sed -i 's/luci-lib-ipkg/luci-base/g' package/gd772/luci-app-bypass/Makefile
+
 # 调整 Alist 文件列表 到 系统 菜单
 #sed -i 's/nas/system/g' package/alist/luci-app-alist/luasrc/controller/*.lua
 #sed -i 's/nas/system/g' package/alist/luci-app-alist/luasrc/model/cbi/alist/*.lua
