d9afe0620688cc069e89d9ae0aa76e7fab04185c
0118Add <60157610+0118Add@users.noreply.github.com>
2022-10-12 20:56:14 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -28,8 +28,12 @@ sed -i 's/"Argon 主题设置"/"主题设置"/g' package/luci-app-argon-config/p
 git clone https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial_new package/luci-theme-atmaterial_new
-#git clone https://github.com/sbwml/luci-app-alist.git package/alist
-#sed -i 's/Alist 文件列表/文件列表/g' package/alist/luci-app-alist/po/zh-cn/alist.po
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-speedlimit package/luci-app-control-speedlimit
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-timewol package/luci-app-control-timewol
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-webrestriction package/luci-app-control-webrestriction
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-control-weburl package/luci-app-control-weburl
+git clone https://github.com/sbwml/luci-app-alist.git package/alist
+sed -i 's/Alist 文件列表/文件列表/g' package/alist/luci-app-alist/po/zh-cn/alist.po
 git clone https://github.com/ophub/luci-app-amlogic.git package/amlogic
 wget https://raw.githubusercontent.com/0118Add/patch/main/n1.sh
 bash n1.sh
