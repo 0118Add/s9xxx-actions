7192942eca1f945d4c48f894f7297e96ac053048
0118Add <60157610+0118Add@users.noreply.github.com>
2026-02-21 22:02:49 +0800

    Update buildImageX.sh

--- docker/buildImageX.sh
+++ docker/buildImageX.sh
@@ -13,7 +13,7 @@ IMG_NAME=0118add/openwrt-aarch64
 
 mkdir -p "$TMPDIR"  && \
 mkdir -p "$OUTDIR"  && \
-gzip -dc immortalwrt-armsr-armv8-generic-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
+gzip -dc openwrt-armsr-armv8-generic-rootfs.tar.gz | ( cd "$TMPDIR" && tar xf - ) && \
 cp -f patchesx/rc.local "$TMPDIR/etc/" && \
 cp -f patchesx/cpustat "$TMPDIR/usr/bin/" && chmod 755 "$TMPDIR/usr/bin/cpustat" && \
 cp -f patchesx/getcpu "$TMPDIR/bin/" && chmod 755 "$TMPDIR/bin/getcpu" && \
@@ -26,10 +26,10 @@ ddd=$((sss/86400)) && \
 sed -e "s/:0:0:99999:7:::/:${ddd}:0:99999:7:::/" -i "${TMPDIR}/etc/shadow" && \
 echo "17 3 * * * /etc/coremark.sh" >> "$TMPDIR/etc/crontabs/root" && \
 rm -rf "$TMPDIR/lib/firmware/*" "$TMPDIR/lib/modules/*" && \
-(cd "$TMPDIR" && tar cf ../immortalwrt-armsr-armv8-generic-rootfs-patched.tar .) && \
+(cd "$TMPDIR" && tar cf ../openwrt-armsr-armv8-generic-rootfs-patched.tar .) && \
 rm -f DockerImg-OpenwrtArm64-${TAG}.gz && \
 docker build -t ${IMG_NAME}:${TAG} . && \
 docker buildx build --platform=linux/arm64 -o type=docker -t ${IMG_NAME}:${TAG} . && \
-rm -f  immortalwrt-armsr-armv8-generic-rootfs-patched.tar && \
+rm -f  openwrt-armsr-armv8-generic-rootfs-patched.tar && \
 rm -rf "$TMPDIR" && \
 docker save ${IMG_NAME}:${TAG} | pigz -9 > $OUTDIR/docker-img-openwrt-aarch64-${TAG}.gz
