a5e6e736bf24c731dd828e40363034dda884231b
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-15 18:11:57 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 自定义默认网关
-sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/10.0.0.10/g' package/base-files/files/bin/config_generate
 
 rm -rf package/emortal/automount
 rm -rf feeds/packages/admin/zabbix
